2a77767bf6a13c14076c5f56bed644139f2247a5
alex-clark <ahclark@gmail.com>
2013-09-13 15:17:00 +0100

    Added option to exclude orig fields if a std field is present
    Cuts down outputted csv to essential values if required

--- Gedcomx.CLI/RecordSetTransformer.cs
+++ Gedcomx.CLI/RecordSetTransformer.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Gx.Records;
 using System.Xml.Serialization;
 using Gx.Util;
@@ -25,11 +27,14 @@ namespace Gx.CLI
         /// <param name="statusWriter">
         /// Where to write the status of the procedure
         /// </param>
-        public static void WriteCSV(String inFile, TextWriter outWriter, TextWriter statusWriter)
+        /// <param name="excludeOrigColumns">
+        /// Should the output include the original values as well as the standarized ones
+        /// </param>
+        public static void WriteCSV(String inFile, TextWriter outWriter, TextWriter statusWriter, bool excludeOrigColumns)
         {
             try
             {
-                WriteCSV (new FileStream (inFile, FileMode.Open), outWriter);
+                WriteCSV(new FileStream(inFile, FileMode.Open), outWriter, excludeOrigColumns);
                 statusWriter.WriteLine("Finished");
             }
             catch (Exception exception)
@@ -38,7 +43,7 @@ namespace Gx.CLI
             }
             
         }
-     
+
         /// <summary>
         /// Reads a record set file and outputs a CSV.
         /// </summary>
@@ -48,11 +53,37 @@ namespace Gx.CLI
         /// <param name='outWriter'>
         /// Where to write the CSV.
         /// </param>
-        public static void WriteCSV (Stream inStream, TextWriter outWriter)
+        /// <param name="excludeOrigColumns">
+        /// Should the output include the original values as well as the standarized ones
+        /// </param>
+        public static void WriteCSV (Stream inStream, TextWriter outWriter, bool excludeOrigColumns)
         {
             var serializer = new XmlSerializer (typeof(RecordSet));
             var records = (RecordSet)serializer.Deserialize (inStream);
             var table = RecordHelper.BuildTableOfRecords (records);
+
+            if (excludeOrigColumns)
+            {
+                var collectionFieldsToRemove = new List<DataColumn>();
+                foreach (var collectionField in table.Columns.Cast<DataColumn>().Where(x => x.ColumnName.EndsWith("_ORIG")))
+                {
+                    if (
+                        table.Columns.Cast<DataColumn>().Any(
+                            x =>
+                            x.ColumnName ==
+                            collectionField.ColumnName.Substring(0, collectionField.ColumnName.Length - 5) + "_STD" 
+                                || x.ColumnName == collectionField.ColumnName.Substring(0, collectionField.ColumnName.Length - 5)))
+                    {
+                        collectionFieldsToRemove.Add(collectionField);
+                    }
+                }
+
+                foreach (var dataColumn in collectionFieldsToRemove)
+                {
+                    table.Columns.Remove(dataColumn);    
+                }
+            }
+
             var columnCount = table.Columns.Count;
             foreach (DataColumn column in table.Columns) {
                 outWriter.Write (column.ColumnName);

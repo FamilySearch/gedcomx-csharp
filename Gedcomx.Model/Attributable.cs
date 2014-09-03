ea9f494be545204a10b00b777bcc8c328a1b63d9
shane.walters@live.com <shane.walters@live.com>
2014-09-03 17:05:27 -0600

    Added missing file references.

---
+++ Gedcomx.Model/Attributable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gx.Common
+{
+    public interface Attributable
+    {
+        /// <summary>
+        /// Attribution metadata for a genealogical resource. Attribution data is necessary to support
+        /// a sound <a href="https://wiki.familysearch.org/en/Genealogical_Proof_Standard">genealogical proof statement</a>.
+        /// </summary>
+        /// <value>
+        /// Attribution metadata for a genealogical resource. Attribution data is necessary to support
+        /// a sound <a href="https://wiki.familysearch.org/en/Genealogical_Proof_Standard">genealogical proof statement</a>.
+        /// </value>
+        Attribution Attribution { get; set; }
+    }
+}
---
+++ Gedcomx.Model/Util/JsonUnixTimestampConverter.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gedcomx.Model.Util
+{
+    public class JsonUnixTimestampConverter : JsonConverter
+    {
+        DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(long);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return epoch.AddMilliseconds((long)reader.Value);
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteValue(Convert.ToInt64(((DateTime)value - epoch).TotalMilliseconds));
+        }
+    }
+}

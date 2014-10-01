f3126e4eca45a42bf4a8c99dbc2da1af1e9f19ce
shane.walters@live.com <shane.walters@live.com>
2014-10-01 07:05:51 -0600

    GedcomxDateRange port.

--- GEDCOM X Date/GedcomxDateRange.cs
+++ GEDCOM X Date/GedcomxDateRange.cs
@@ -5,29 +5,190 @@ using System.Text;
 
 namespace Gedcomx.Date
 {
-    class GedcomxDateRange : GedcomxDate
+    public class GedcomxDateRange : GedcomxDate
     {
-        private string date;
+        private bool approximate = false;
+        private GedcomxDateSimple start = null;
+        private GedcomxDateDuration duration = null;
+        private GedcomxDateSimple end = null;
 
-        public GedcomxDateRange(string date)
+        /**
+         * Instantiate a new Range date from the formal string
+         * @param date The formal date string
+         */
+        public GedcomxDateRange(String date)
         {
-            // TODO: Complete member initialization
-            this.date = date;
+
+            if (date == null || date.Length < 1)
+            {
+                throw new GedcomxDateException("Invalid Range");
+            }
+
+            String range = date;
+
+            // If range starts with A it is recurring
+            if (date[0] == 'A')
+            {
+                approximate = true;
+                range = date.Substring(1);
+            }
+
+            // / is required
+            if (!date.Contains("/"))
+            {
+                throw new GedcomxDateException("Invalid Range: / is required");
+            }
+
+            /**
+             * range -> parts
+             * / -> []
+             * +1000/ -> ["+1000"]
+             * /+1000 -> ["","+1000"]
+             * +1000/+2000 -> ["+1000","+2000"]
+             */
+            String[] parts = range.Split('/');
+
+            if (parts.Length < 1 || parts.Length > 2)
+            {
+                throw new GedcomxDateException("Invalid Range: One or two parts are required");
+            }
+
+            if (!parts[0].Equals(""))
+            {
+                try
+                {
+                    start = new GedcomxDateSimple(parts[0]);
+                }
+                catch (GedcomxDateException e)
+                {
+                    throw new GedcomxDateException(e.Message + " in Range Start Date");
+                }
+            }
+
+            if (parts.Length == 2)
+            {
+                if (parts[1][0] == 'P')
+                {
+                    if (start == null)
+                    {
+                        throw new GedcomxDateException("Invalid Range: A range may not end with a duration if missing a start date");
+                    }
+                    try
+                    {
+                        duration = new GedcomxDateDuration(parts[1]);
+                    }
+                    catch (GedcomxDateException e)
+                    {
+                        throw new GedcomxDateException(e.Message + " in Range End Duration");
+                    }
+                    // Use the duration to calculate the end date
+                    end = GedcomxDateUtil.AddDuration(start, duration);
+                }
+                else
+                {
+                    try
+                    {
+                        end = new GedcomxDateSimple(parts[1]);
+                    }
+                    catch (GedcomxDateException e)
+                    {
+                        throw new GedcomxDateException(e.Message + " in Range End Date");
+                    }
+                    if (start != null)
+                    {
+                        duration = GedcomxDateUtil.GetDuration(start, end);
+                    }
+                }
+            }
         }
 
+        /**
+         * Get the start of the range
+         * @return The Start Date
+         */
+        public GedcomxDateSimple Start
+        {
+            get
+            {
+                return start;
+            }
+        }
+
+        /**
+         * Get the duration between the start and end dates
+         * @return The Duration
+         */
+        public GedcomxDateDuration Duration
+        {
+            get
+            {
+                return duration;
+            }
+        }
+
+        /**
+         * Get the end of the range
+         * @return The End Date
+         */
+        public GedcomxDateSimple End
+        {
+            get
+            {
+                return end;
+            }
+        }
+
+        /**
+         * Get the type of this date
+         * @return The Date Type
+         */
         public override GedcomxDateType Type
         {
-            get { throw new NotImplementedException(); }
+            get
+            {
+                return GedcomxDateType.RANGE;
+            }
         }
 
+        /**
+         * Whether or not this date is considered approximate
+         * @return True if it this date is approximate
+         */
         public override bool IsApproximate()
         {
-            throw new NotImplementedException();
+            return approximate;
         }
 
-        public override string ToFormalString()
+        /**
+         * Return the formal string for this date
+         * @return The formal string
+         */
+        public override String ToFormalString()
         {
-            throw new NotImplementedException();
+            StringBuilder range = new StringBuilder();
+
+            if (approximate)
+            {
+                range.Append('A');
+            }
+
+            if (start != null)
+            {
+                range.Append(start.ToFormalString());
+            }
+
+            range.Append('/');
+
+            if (duration != null)
+            {
+                range.Append(duration.ToFormalString());
+            }
+            else if (end != null)
+            {
+                range.Append(end.ToFormalString());
+            }
+
+            return range.ToString();
         }
     }
 }

ebd6857b551d34c33f9afd2aa19149ab6a1640a7
shane.walters@live.com <shane.walters@live.com>
2014-10-10 19:10:42 -0600

    Added redirect tests.

--- Gedcomx.Rs.Api.Test/UtilitiesTests.cs
+++ Gedcomx.Rs.Api.Test/UtilitiesTests.cs
@@ -12,25 +12,36 @@ using Gedcomx.Support;
 using Newtonsoft.Json.Linq;
 using FamilySearch.Api.Ft;
 using Newtonsoft.Json;
+using System.Net;
 
 namespace Gedcomx.Rs.Api.Test
 {
     [TestFixture]
     public class UtilitiesTests
     {
+        private FamilySearchFamilyTree tree;
+
+        [TestFixtureSetUp]
+        public void Initialize()
+        {
+            tree = new FamilySearchFamilyTree(true);
+            tree.AuthenticateViaOAuth2Password("sdktester", "1234sdkpass", "WCQY-7J1Q-GKVV-7DNM-SQ5M-9Q5H-JX3H-CMJK");
+        }
+
         [Test]
         public void TestReadPersonWithMultiplePendingModificationsActivated()
         {
-            var tree = new FamilySearchFamilyTree(true);
+            // Make a separate copy from the main tree used in other tests
+            var tempTree = new FamilySearchFamilyTree(true);
             var features = new List<String>();
 
-            tree.AuthenticateViaOAuth2Password("sdktester", "1234sdkpass", "WCQY-7J1Q-GKVV-7DNM-SQ5M-9Q5H-JX3H-CMJK");
+            tempTree.AuthenticateViaOAuth2Password("sdktester", "1234sdkpass", "WCQY-7J1Q-GKVV-7DNM-SQ5M-9Q5H-JX3H-CMJK");
 
             // Get all the features that are pending
             IRestRequest request = new RestRequest()
                 .Accept(MediaTypes.APPLICATION_JSON_TYPE)
                 .Build("https://sandbox.familysearch.org/platform/pending-modifications", Method.GET);
-            IRestResponse response = tree.Client.Handle(request);
+            IRestResponse response = tempTree.Client.Handle(request);
 
             // Get each pending feature
             foreach (var kvp in JsonConvert.DeserializeObject<IDictionary<String, JToken>>(response.Content).Where(x => x.Key == "features"))
@@ -42,9 +53,9 @@ namespace Gedcomx.Rs.Api.Test
             }
 
             // Add every pending feature to the tree's current client
-            tree.Client.AddFilter(new ExperimentsFilter(features.ToArray()));
+            tempTree.Client.AddFilter(new ExperimentsFilter(features.ToArray()));
 
-            var state = tree.AddPerson(TestBacking.GetCreateMalePerson());
+            var state = tempTree.AddPerson(TestBacking.GetCreateMalePerson());
 
             // Ensure a response came back
             Assert.IsNotNull(state);
@@ -56,7 +67,6 @@ namespace Gedcomx.Rs.Api.Test
         [Test]
         public void TestReadPersonWithPendingModificationActivated()
         {
-            var tree = new FamilySearchFamilyTree(true);
             // The default client is assumed to add a single pending feature (if it doesn't, this test will fail)
             var state = tree.AuthenticateViaOAuth2Password("sdktester", "1234sdkpass", "WCQY-7J1Q-GKVV-7DNM-SQ5M-9Q5H-JX3H-CMJK");
 
@@ -65,6 +75,60 @@ namespace Gedcomx.Rs.Api.Test
             Assert.IsNotNull(requestedFeatures);
             Assert.AreEqual(-1, requestedFeatures.IndexOf(","));
             Assert.Greater(requestedFeatures.Length, 0);
+        }
+
+        [Test]
+        public void TestRedirectToPerson()
+        {
+            var person = tree.AddPerson(TestBacking.GetCreateMalePerson());
+            var id = person.Response.Headers.Get("X-ENTITY-ID").Single().Value.ToString();
+            IRestRequest request = new RestRequest()
+                .Accept(MediaTypes.APPLICATION_JSON_TYPE)
+                .Build("https://sandbox.familysearch.org/platform/redirect?person=" + id, Method.GET);
+            var response = tree.Client.Execute(request);
+
+            Assert.IsNotNull(response);
+            Assert.AreEqual(HttpStatusCode.TemporaryRedirect, response.StatusCode);
+        }
+
+        [Test]
+        public void TestRedirectToPersonMemories()
+        {
+            var person = tree.AddPerson(TestBacking.GetCreateMalePerson());
+            var id = person.Response.Headers.Get("X-ENTITY-ID").Single().Value.ToString();
+            IRestRequest request = new RestRequest()
+                .Accept(MediaTypes.APPLICATION_JSON_TYPE)
+                .Build("https://sandbox.familysearch.org/platform/redirect?context=memories&person=" + id, Method.GET);
+            var response = tree.Client.Execute(request);
+
+            Assert.IsNotNull(response);
+            Assert.AreEqual(HttpStatusCode.TemporaryRedirect, response.StatusCode);
+        }
+
+        [Test]
+        public void TestRedirectToSourceLinker()
+        {
+            var person = (FamilyTreePersonState)tree.AddPerson(TestBacking.GetCreateMalePerson()).Get();
+            var uri = String.Format("https://sandbox.familysearch.org/platform/redirect?context=sourcelinker&person={0}&hintId={1}", person.Person.Id, person.Person.Identifiers[0].Value);
+            IRestRequest request = new RestRequest()
+                .Accept(MediaTypes.APPLICATION_JSON_TYPE)
+                .Build(uri, Method.GET);
+            var response = tree.Client.Execute(request);
+
+            Assert.IsNotNull(response);
+            Assert.AreEqual(HttpStatusCode.TemporaryRedirect, response.StatusCode);
+        }
+
+        [Test]
+        public void TestRedirectToUri()
+        {
+            IRestRequest request = new RestRequest()
+                .Accept(MediaTypes.APPLICATION_JSON_TYPE)
+                .Build("https://sandbox.familysearch.org/platform/redirect?uri=https://familysearch.org/some/path?p1%3Dp1-value%26p2%3Dp2-value", Method.GET);
+            var response = tree.Client.Execute(request);
+
+            Assert.IsNotNull(response);
+            Assert.AreEqual(HttpStatusCode.TemporaryRedirect, response.StatusCode);
         }
     }
 }

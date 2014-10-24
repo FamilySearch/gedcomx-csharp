92553c461dc874de527fc182553e931b0c1580a8
shane.walters@live.com <shane.walters@live.com>
2014-10-24 11:55:43 -0600

    Added new example test.

---
+++ Gedcomx.Model/NamePartQualifierType.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gx.Types
+{
+    public enum NamePartQualifierType
+    {
+
+        /**
+         * A designation for honorifics (e.g. Dr., Rev., His Majesty, Haji),
+         * ranks (e.g. Colonel, General, Knight, Esquire),
+         * positions (e.g. Count, Chief, Father, King) or other titles (e.g., PhD, MD)
+         */
+        [System.Xml.Serialization.XmlEnum("http://gedcomx.org/Title")]
+        Title,
+
+        /**
+         * A designation for the name of most prominent in importance among the names of that type (e.g., the primary given name).
+         */
+        [System.Xml.Serialization.XmlEnum("http://gedcomx.org/Primary")]
+        Primary,
+
+        /**
+         * A designation for a name that is not primary in its importance among the names of that type (e.g., a secondary given name).
+         */
+        [System.Xml.Serialization.XmlEnum("http://gedcomx.org/Secondary")]
+        Secondary,
+
+        /**
+         * A designation useful for cultures that designate a middle name that is distinct from a given name and a surname.
+         */
+        [System.Xml.Serialization.XmlEnum("http://gedcomx.org/Middle")]
+        Middle,
+
+        /**
+         * A designation for one's familiar name.
+         */
+        [System.Xml.Serialization.XmlEnum("http://gedcomx.org/Familiar")]
+        Familiar,
+
+        /**
+         * A designation for a name given for religious purposes.
+         */
+        [System.Xml.Serialization.XmlEnum("http://gedcomx.org/Religious")]
+        Religious,
+
+        /**
+         * A name that associates a person with a group, such as a clan, tribe, or patriarchal hierarchy.
+         */
+        [System.Xml.Serialization.XmlEnum("http://gedcomx.org/Family")]
+        Family,
+
+        /**
+         * A designation given by women to their original surname after they adopt a new surname upon marriage.
+         */
+        [System.Xml.Serialization.XmlEnum("http://gedcomx.org/Maiden")]
+        Maiden,
+
+        /**
+         * A name derived from a father or paternal ancestor.
+         */
+        [System.Xml.Serialization.XmlEnum("http://gedcomx.org/Patronymic")]
+        Patronymic,
+
+        /**
+         * A name derived from a mother or maternal ancestor.
+         */
+        [System.Xml.Serialization.XmlEnum("http://gedcomx.org/Matronymic")]
+        Matronymic,
+
+        /**
+         * A name derived from associated geography.
+         */
+        [System.Xml.Serialization.XmlEnum("http://gedcomx.org/Geographic")]
+        Geographic,
+
+        /**
+         * A name derived from one's occupation.
+         */
+        [System.Xml.Serialization.XmlEnum("http://gedcomx.org/Occupational")]
+        Occupational,
+
+        /**
+         * A name derived from a characteristic.
+         */
+        [System.Xml.Serialization.XmlEnum("http://gedcomx.org/Characteristic")]
+        Characteristic,
+
+        /**
+         * A name mandedated by law populations from Congo Free State / Belgian Congo / Congo / Democratic Republic of Congo (formerly Zaire).
+         */
+        [System.Xml.Serialization.XmlEnum("http://gedcomx.org/Postnom")]
+        Postnom,
+
+        /**
+         * A grammatical designation for articles (a, the, dem, las, el, etc.),
+         * prepositions (of, from, aus, zu, op, etc.), initials (e.g. PhD, MD),
+         * annotations (e.g. twin, wife of, infant, unknown),
+         * comparators (e.g. Junior, Senior, younger, little), ordinals (e.g. III, eighth),
+         * and conjunctions (e.g. and, or, nee, ou, y, o, ne, &amp;).
+         */
+        [System.Xml.Serialization.XmlEnum("http://gedcomx.org/Particle")]
+        Particle,
+
+        [System.Xml.Serialization.XmlEnum("http://gedcomx.org/OTHER")]
+        OTHER
+    }
+}
---
+++ Gedcomx.Rs.Api.Test/Examples/NamesExampleTest.cs
@@ -0,0 +1,110 @@
+﻿using Gedcomx.File;
+using Gx.Common;
+using Gx.Conclusion;
+using Gx.Types;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gedcomx.Rs.Api.Test.Examples
+{
+    [TestFixture]
+    public class NamesExampleTest
+    {
+        private DefaultXmlSerialization xmlSerializer;
+        private DefaultJsonSerialization jsonSerializer;
+
+        [TestFixtureSetUp]
+        public void Initialize()
+        {
+            xmlSerializer = new DefaultXmlSerialization();
+            jsonSerializer = new DefaultJsonSerialization();
+        }
+
+        [Test]
+        public void TestBasicWesternName()
+        {
+            NameForm nameForm = new NameForm("John Fitzgerald Kennedy")
+              .SetLang("en")
+              .SetPart(NamePartType.Given, "John")
+              .SetPart(NamePartType.Given, "Fitzgerald")
+              .SetPart(NamePartType.Surname, "Kennedy");
+            Name name = new Name().SetNameForm(nameForm);
+
+            Gx.Gedcomx gx = new Gx.Gedcomx().SetPerson(new Person().SetName(name));
+            xmlSerializer.Deserialize<Gx.Gedcomx>(xmlSerializer.Serialize(gx));
+            jsonSerializer.Deserialize<Gx.Gedcomx>(jsonSerializer.Serialize(gx));
+        }
+
+        [Test]
+        public void TestMultipleJapaneseForms()
+        {
+            NameForm kanji = new NameForm("山田太郎")
+              .SetLang("ja-Hani")
+              .SetPart(NamePartType.Surname, "山田")
+              .SetPart(NamePartType.Given, "太郎");
+            NameForm katakana = new NameForm("ヤマダタロー")
+              .SetLang("ja-Kana")
+              .SetPart(NamePartType.Surname, "ヤマダ")
+              .SetPart(NamePartType.Given, "タロー");
+            NameForm romanized = new NameForm("Yamada Tarō")
+              .SetLang("ja-Latn")
+              .SetPart(NamePartType.Surname, "Tarō")
+              .SetPart(NamePartType.Given, "Yamada");
+            Name name = new Name().SetNameForm(kanji).SetNameForm(katakana).SetNameForm(romanized);
+
+            Gx.Gedcomx gx = new Gx.Gedcomx().SetPerson(new Person().SetName(name));
+            xmlSerializer.Deserialize<Gx.Gedcomx>(xmlSerializer.Serialize(gx));
+            jsonSerializer.Deserialize<Gx.Gedcomx>(jsonSerializer.Serialize(gx));
+        }
+
+        [Test]
+        public void TestMultipleNamePartsOnePartPerType()
+        {
+            NameForm nameForm = new NameForm("José Eduardo Santos Tavares Melo Silva")
+              .SetLang("pt-BR")
+              .SetPart(NamePartType.Given, "José Eduardo")
+              .SetPart(NamePartType.Surname, "Santos Tavares Melo Silva");
+            Name name = new Name().SetNameForm(nameForm);
+
+            Gx.Gedcomx gx = new Gx.Gedcomx().SetPerson(new Person().SetName(name));
+            xmlSerializer.Deserialize<Gx.Gedcomx>(xmlSerializer.Serialize(gx));
+            jsonSerializer.Deserialize<Gx.Gedcomx>(jsonSerializer.Serialize(gx));
+        }
+
+        [Test]
+        public void TestMultipleNamePartsMultiplePartsPerType()
+        {
+            NameForm nameForm = new NameForm("José Eduardo Santos Tavares Melo Silva")
+              .SetLang("pt-BR")
+              .SetPart(NamePartType.Given, "José")
+              .SetPart(NamePartType.Given, "Eduardo")
+              .SetPart(NamePartType.Surname, "Santos")
+              .SetPart(NamePartType.Surname, "Tavares")
+              .SetPart(NamePartType.Surname, "Melo")
+              .SetPart(NamePartType.Surname, "Silva");
+            Name name = new Name().SetNameForm(nameForm);
+
+            Gx.Gedcomx gx = new Gx.Gedcomx().SetPerson(new Person().SetName(name));
+            xmlSerializer.Deserialize<Gx.Gedcomx>(xmlSerializer.Serialize(gx));
+            jsonSerializer.Deserialize<Gx.Gedcomx>(jsonSerializer.Serialize(gx));
+        }
+
+        [Test]
+        public void TestPatronymic()
+        {
+            NameForm nameForm = new NameForm("Björk Guðmundsdóttir")
+              .SetLang("is")
+              .SetPart(NamePartType.Given, "Björk")
+              .SetPart(new NamePart().SetValue("Guðmundsdóttir").SetQualifier(new Qualifier(NamePartQualifierType.Patronymic)));
+            Name name = new Name().SetNameForm(nameForm);
+            
+            Gx.Gedcomx gx = new Gx.Gedcomx().SetPerson(new Person().SetName(name));
+            xmlSerializer.Deserialize<Gx.Gedcomx>(xmlSerializer.Serialize(gx));
+            jsonSerializer.Deserialize<Gx.Gedcomx>(jsonSerializer.Serialize(gx));
+        }
+    }
+}

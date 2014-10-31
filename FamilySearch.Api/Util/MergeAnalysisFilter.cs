f40fbf844bfb3c16bbad1f5c6c72f76c9afa942c
shane.walters@live.com <shane.walters@live.com>
2014-10-31 15:30:04 -0600

    Added XML documentation.

--- FamilySearch.Api/Util/MergeAnalysisFilter.cs
+++ FamilySearch.Api/Util/MergeAnalysisFilter.cs
@@ -6,13 +6,34 @@ using System.Threading.Tasks;
 
 namespace FamilySearch.Api.Util
 {
+    /// <summary>
+    /// Represents the type of merge analysis to filter by.
+    /// </summary>
     public enum MergeAnalysisFilter
     {
+        /// <summary>
+        /// Only the couple relationships merge analysis should be returned.
+        /// </summary>
         CoupleRelationships,
+        /// <summary>
+        /// Only the non vital conclusions merge analysis should be returned.
+        /// </summary>
         NonVitalConclusions,
+        /// <summary>
+        /// Only the parent child relationships as child merge analysis should be returned.
+        /// </summary>
         ParentChildRelationshipsAsChild,
+        /// <summary>
+        /// Only the parent child relationships as parent merge analysis should be returned.
+        /// </summary>
         ParentChildRelationshipsAsParent,
+        /// <summary>
+        /// Only the source references merge analysis should be returned.
+        /// </summary>
         SourceReferences,
+        /// <summary>
+        /// Only the vital conclusions merge analysis should be returned.
+        /// </summary>
         VitalConclusions
     }
 }

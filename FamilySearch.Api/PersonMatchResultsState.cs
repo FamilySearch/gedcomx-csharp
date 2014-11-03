065b53d1ae45f33418a674c154851982badb6aa4
shane.walters@live.com <shane.walters@live.com>
2014-11-03 10:58:38 -0700

    Added XML documentation.

--- FamilySearch.Api/PersonMatchResultsState.cs
+++ FamilySearch.Api/PersonMatchResultsState.cs
@@ -15,8 +15,19 @@ using Gedcomx.Support;
 
 namespace FamilySearch.Api
 {
+    /// <summary>
+    /// The PersonMatchResultsState exposes management functions for a person match results.
+    /// </summary>
     public class PersonMatchResultsState : PersonSearchResultsState
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PersonMatchResultsState"/> class.
+        /// </summary>
+        /// <param name="request">The REST API request that will be used to instantiate this state instance.</param>
+        /// <param name="response">The REST API response that was produced from the REST API request.</param>
+        /// <param name="client">The REST API client to use for API calls.</param>
+        /// <param name="accessToken">The access token to use for subsequent invocations of the REST API client.</param>
+        /// <param name="stateFactory">The state factory to use for state instantiation.</param>
         protected internal PersonMatchResultsState(IRestRequest request, IRestResponse response, IFilterableRestClient client, String accessToken, FamilySearchStateFactory stateFactory)
             : base(request, response, client, accessToken, stateFactory)
         {
@@ -34,6 +45,13 @@ namespace FamilySearch.Api
             return new PersonMatchResultsState(request, response, client, this.CurrentAccessToken, (FamilySearchStateFactory)this.stateFactory);
         }
 
+        /// <summary>
+        /// Reads the current person for these potential match results.
+        /// </summary>
+        /// <param name="options">The options to apply before executing the REST API call.</param>
+        /// <returns>
+        /// A <see cref="PersonState"/> instance containing the REST API response.
+        /// </returns>
         public PersonState ReadPerson(params StateTransitionOption[] options)
         {
             Link link = GetLink(Rel.PERSON);
@@ -46,6 +64,14 @@ namespace FamilySearch.Api
             return ((FamilySearchStateFactory)this.stateFactory).NewPersonStateInt(request, Invoke(request, options), this.Client, this.CurrentAccessToken);
         }
 
+        /// <summary>
+        /// Reads merge options for the specified search result from <see cref="P:Results.Entries"/>.
+        /// </summary>
+        /// <param name="entry">A search result entry from <see cref="P:Results.Entries"/>.</param>
+        /// <param name="options">The options to apply before executing the REST API call.</param>
+        /// <returns>
+        /// A <see cref="PersonMergeState"/> instance containing the REST API response.
+        /// </returns>
         public PersonMergeState ReadMergeOptions(Gx.Atom.Entry entry, params StateTransitionOption[] options)
         {
             Link link = entry.GetLink(Rel.MERGE);
@@ -58,6 +84,14 @@ namespace FamilySearch.Api
             return ((FamilySearchStateFactory)this.stateFactory).NewPersonMergeState(request, Invoke(request, options), this.Client, this.CurrentAccessToken);
         }
 
+        /// <summary>
+        /// Creates a merge analysis for the current person and the potential duplicate person specified by the search result entry from <see cref="P:Results.Entries"/>.
+        /// </summary>
+        /// <param name="entry">The search result entry to perform the merge analysis upon.</param>
+        /// <param name="options">The options to apply before executing the REST API call.</param>
+        /// <returns>
+        /// A <see cref="PersonMergeState"/> instance containing the REST API response.
+        /// </returns>
         public PersonMergeState ReadMergeAnalysis(Gx.Atom.Entry entry, params StateTransitionOption[] options)
         {
             Link link = entry.GetLink(Rel.MERGE);
@@ -70,6 +104,14 @@ namespace FamilySearch.Api
             return ((FamilySearchStateFactory)this.stateFactory).NewPersonMergeState(request, Invoke(request, options), this.Client, this.CurrentAccessToken);
         }
 
+        /// <summary>
+        /// Declares the specified search result entry (from <see cref="P:Results.Entries"/>) as not a match for the current person.
+        /// </summary>
+        /// <param name="entry">The search result entry from <see cref="P:Results.Entries"/>.</param>
+        /// <param name="options">The options to apply before executing the REST API call.</param>
+        /// <returns>
+        /// A <see cref="PersonNonMatchesState"/> instance containing the REST API response.
+        /// </returns>
         public PersonNonMatchesState AddNonMatch(Gx.Atom.Entry entry, params StateTransitionOption[] options)
         {
             Link link = GetLink(Rel.NOT_A_MATCHES);
@@ -84,6 +126,15 @@ namespace FamilySearch.Api
             return ((FamilySearchStateFactory)this.stateFactory).NewPersonNonMatchesState(request, Invoke(request, options), this.Client, this.CurrentAccessToken);
         }
 
+        /// <summary>
+        /// Declares the match status for the current person the specified search result entry from <see cref="P:Results.Entries"/>.
+        /// </summary>
+        /// <param name="entry">The search result entry (from <see cref="P:Results.Entries"/>) to have the match status updated.</param>
+        /// <param name="status">The new status to apply to the specified search result entry from <see cref="P:Results.Entries"/>.</param>
+        /// <param name="options">The options to apply before executing the REST API call.</param>
+        /// <returns>
+        /// A <see cref="PersonMatchResultsState"/> instance containing the REST API response.
+        /// </returns>
         public PersonMatchResultsState UpdateMatchStatus(Gx.Atom.Entry entry, MatchStatus status, params StateTransitionOption[] options)
         {
             String updateStatusUri = GetSelfUri().SetQueryParam(FamilySearchOptions.STATUS, status.ToString().ToLower()).ToString();
--- FamilySearch.Api/PersonNonMatchesState.cs
+++ FamilySearch.Api/PersonNonMatchesState.cs
@@ -11,8 +11,19 @@ using FamilySearch.Api.Util;
 
 namespace FamilySearch.Api
 {
+    /// <summary>
+    /// The PersonNonMatchesState exposes management functions for person non matches.
+    /// </summary>
     public class PersonNonMatchesState : PersonsState
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PersonNonMatchesState"/> class.
+        /// </summary>
+        /// <param name="request">The REST API request that will be used to instantiate this state instance.</param>
+        /// <param name="response">The REST API response that was produced from the REST API request.</param>
+        /// <param name="client">The REST API client to use for API calls.</param>
+        /// <param name="accessToken">The access token to use for subsequent invocations of the REST API client.</param>
+        /// <param name="stateFactory">The state factory to use for state instantiation.</param>
         protected internal PersonNonMatchesState(IRestRequest request, IRestResponse response, IFilterableRestClient client, String accessToken, StateFactory stateFactory)
             : base(request, response, client, accessToken, stateFactory)
         {
@@ -30,11 +41,27 @@ namespace FamilySearch.Api
             return new PersonNonMatchesState(request, response, client, this.CurrentAccessToken, this.stateFactory);
         }
 
+        /// <summary>
+        /// Adds a person as a non match to this collection.
+        /// </summary>
+        /// <param name="person">The person that is not a match (from potential duplicate search results).</param>
+        /// <param name="options">The options to apply before executing the REST API call.</param>
+        /// <returns>
+        /// A <see cref="PersonNonMatchesState"/> instance containing the REST API response.
+        /// </returns>
         public PersonNonMatchesState AddNonMatch(Person person, params StateTransitionOption[] options)
         {
             return (PersonNonMatchesState)Post(new Gx.Gedcomx() { Persons = new List<Person>() { person } }, options);
         }
 
+        /// <summary>
+        /// Removes the declared non match person from this collection.
+        /// </summary>
+        /// <param name="nonMatch">The person that was previously declared as a non-match.</param>
+        /// <param name="options">The options to apply before executing the REST API call.</param>
+        /// <returns>
+        /// A <see cref="PersonNonMatchesState"/> instance containing the REST API response.
+        /// </returns>
         public PersonNonMatchesState RemoveNonMatch(Person nonMatch, params StateTransitionOption[] options)
         {
             Link link = nonMatch.GetLink(Rel.NOT_A_MATCH);

7dd7879b240f7a4e5a12d05c7d3ed211516a8d8d
Dan Woodruff <dwoodruff@ldschurch.org>
2016-10-27 11:15:28 -0600

    Add missing enumeration file

---
+++ FamilySearch.Api/EnvironmentEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace FamilySearch.Api
+{
+	/// <summary>
+	/// Deteremines what authentication and api routes to use
+	/// </summary>
+	public enum EnvironmentEnum
+	{
+		Sandbox,
+		Beta,
+		Production
+	}
+}
